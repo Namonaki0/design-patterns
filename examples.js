07327122f388670f61847b24c380c334bdcb7c43
Andre <andreferreira@live.co.uk>
2023-10-05 10:58:21 +0100

    Module pattern

--- examples.js
+++ examples.js
@@ -1,5 +1,4 @@
 //? Singleton Pattern
-
 const Singleton = (() => {
   let instance;
 
@@ -48,7 +47,46 @@ console.log(instance1.publicVariable); // Output: I am a public variable
 console.log(instance1.getPrivateVariable()); // Output: I am a private variable
 console.log(instance1.getPrivateMethod()); // Output: I am a private variable
 
-// Module Pattern
+// ####################################################
+
+//? Module Pattern
+const MyModule = (() => {
+  // Private members
+  let privateVariable = 10;
+
+  const privateMethod = () => {
+    return "Private method";
+  };
+
+  const getPrivateVariable = () => {
+    return privateVariable;
+  };
+
+  // Public members
+  const publicVariable = 20;
+
+  const publicMethod = () => {
+    console.log("Public method");
+    console.log("Accessing private variable:", privateVariable);
+    console.log("Calling private method:", privateMethod());
+  };
+
+  return {
+    // Expose public members
+    publicVariable,
+    publicMethod,
+    getPrivateVariable,
+  };
+})();
+
+// Usage
+console.log(MyModule.publicVariable); // Accessing public variable
+MyModule.publicMethod(); // Calling public method
+
+console.log(MyModule.privateVariable); // Undefined (private variable not accessible)
+console.log(MyModule.privateMethod()); // Error (private method not accessible)
+
+console.log(MyModule.getPrivateVariable()); // Accessing private variable through a public method
 
 // Factory Pattern
 
